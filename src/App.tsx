8d1bd92baa8d99ef64d7d6c57214adfd987ca48e
Carolina Mattos <mattos.carolina98@gmail.com>
2023-11-04 18:27:47 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import Header from './components/Cabecalho'
+import { ThemeProvider } from 'styled-components'
+
+import Cabecalho from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import EstiloGlobal, { Container } from './styles'
+
+import temaClassic from './themes/classic'
 
 function App() {
   return (
-    <>
-      <Header />
+    <ThemeProvider theme={temaClassic}>
+      <EstiloGlobal />
+      <Cabecalho />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { FormHero, HeroTitle, FormContainer } from './style'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <FormHero>
+    <FormContainer>
+      <HeroTitle>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </HeroTitle>
+    </FormContainer>
+  </FormHero>
 )
 
 export default Hero
